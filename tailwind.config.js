23807e8387fd99943499333ee8ed5897edd26ddf
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-08 15:00:48 +0530

    added config for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '576px',   // Replaces default sm (640px)
+      'md': '960px',   // Replaces default md (768px)
+      'lg': '1440px',  // Replaces default lg (1024px)
+    },
+  },
+}
