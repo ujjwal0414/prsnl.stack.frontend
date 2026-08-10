0d45cda453129ac6c14d2c6dbb37f73dcac2dc7b
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-10 21:01:09 +0530

    added sttus data

---
+++ src/constants/status.js
@@ -0,0 +1,18 @@
+const vendorStatus = {
+    "0":{
+        status:"Online",
+        className:"text-green-300"
+    },
+    "1":{
+        status:"Busy",
+        className:"text-yellow-300"
+    },
+    "2":{
+        status:"Offline",
+        className:"text-red-300"
+    },
+    "3":{
+        status:"Out of office",
+        className:"text-purple-300"
+    }
+}
