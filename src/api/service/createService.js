cff5f00ab7a31d2fe4683fdce6995d232055b789
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-19 16:52:37 +0530

    added service function

--- src/api/service/createService.js
+++ src/api/service/createService.js
@@ -1,1 +1,5 @@
 import { axiosHeader } from "../axios.js";
+const createVendorService = async(data)=>{
+    const response = await axiosHeader.post("/vendor/createService,data");
+    return response?.data
+}
