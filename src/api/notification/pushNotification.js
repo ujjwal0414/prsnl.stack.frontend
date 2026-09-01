159ba1effd81dcc3ee17ebd66bc98b262b57795f
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-09-01 12:57:10 +0530

    added notify section

---
+++ src/api/notification/pushNotification.js
@@ -0,0 +1,5 @@
+import { axiosHeader } from "../axios";
+const pushNotification = async(data)=>{
+    const response = await axiosHeader.post("/admin/pushNotification",data);
+    return response?.data
+}
